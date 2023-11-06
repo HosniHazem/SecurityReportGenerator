79413358b8cf4e7110ec8ad54d40e7131dfc55e7
ahmedbenbettaieb <ahmed.benbettaieb@isimg.tn>
2023-11-06 15:31:47 +0100

    fixing the relationship between glbpip & customer

--- backend/app/Http/Controllers/GlbPipController.php
+++ backend/app/Http/Controllers/GlbPipController.php
@@ -6,6 +6,7 @@ use App\Models\GlbPip;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
+use App\Models\Customer;
 
 class GlbPipController extends Controller
 {
@@ -49,47 +50,56 @@ class GlbPipController extends Controller
      */
     //tested  with sucess 
     public function store(Request $request)
-    {
-       // Validate the incoming request data
-       $validator = Validator::make($request ->all(),[
+{
+    // Validate the incoming request data
+    $validator = Validator::make($request->all(), [
         'Nom' => 'required',
         'Titre' => 'required',
         'adresse_mail_primaire' => 'required|email',
         'adresse_mail_secondaire' => 'email',
         'tel' => 'required',
+        'customer_id' => 'required|exists:customers,id', // Ensure customer_id exists in the customers table
     ]);
+
     if ($validator->fails()) {
         return response()->json([
             'status' => 422,
             'message' => $validator->getMessageBag(),
-
         ]);
     }
-        
-        $existingGlbPip = GlbPip::where('Adresse mail primaire', $request->input('adresse_mail_primaire'))
+
+    $existingGlbPip = GlbPip::where('Adresse mail primaire', $request->input('adresse_mail_primaire'))
         ->orWhere('Adresse mail secondaire', $request->input('adresse_mail_primaire'))
         ->first();
 
     if ($existingGlbPip) {
-        return response()->json(['success'=>false,'message' => 'Email address already exists']);
+        return response()->json(['success' => false, 'message' => 'Email address already exists']);
     }
 
-        // Create a new GlbPip instance with the validated data
-        $glbPip = new GlbPip();
-        $glbPip->Nom = $request->input('Nom');
-        $glbPip->Titre = $request->input('Titre');
-        $glbPip["Adresse mail primaire"] = $request->input('adresse_mail_primaire');
-        $glbPip["Adresse mail secondaire"] = $request->input('adresse_mail_secondaire');
-        $glbPip->Tél = $request->input('tel');
-        $glbPip->active = false;
+    // Retrieve the associated customer
+    $customer = Customer::find($request->input('customer_id'));
+
+    if (!$customer) {
+        return response()->json(['success' => false, 'message' => 'Customer not found']);
+    }
+
+    // Create a new GlbPip instance associated with the customer
+    $glbPip = new GlbPip();
+    $glbPip->Nom = $request->input('Nom');
+    $glbPip->Titre = $request->input('Titre');
+    $glbPip["Adresse mail primaire"] = $request->input('adresse_mail_primaire');
+    $glbPip["Adresse mail secondaire"] = $request->input('adresse_mail_secondaire');
+    $glbPip["Tél"] = $request->input('tel');
+    $glbPip->customer()->associate($customer); // Associate the GlbPip with the customer
+    $glbPip->active = true;
 
     // Save the GlbPip record to the database
-        $glbPip->save();
+    $glbPip->save();
 
     // Respond with a success message and the created record
-    return response()->json(['success' => true, 'message' => 'glb pip ajouté avec succées', 'data' => $glbPip], 201);
+    return response()->json(['success' => true, 'message' => 'glb pip ajouté avec succès', 'data' => $glbPip], 201);
+}
 
-    }
 
     /**
      * Display the specified resource.
