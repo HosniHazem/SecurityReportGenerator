7be05c11ff109a2c9ec3d00a346ce2d2f4a72318
ahmedbenbettaieb <ahmed.benbettaieb@isimg.tn>
2023-11-08 08:43:47 +0100

    uploading files + creating customer in one route

--- backend/app/Http/Controllers/CustomerController.php
+++ backend/app/Http/Controllers/CustomerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Customer;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\File;
 
 class CustomerController extends Controller
 {
@@ -47,44 +48,66 @@ class CustomerController extends Controller
         return response()->json(['lastCustomerId' => $lastCustomerId, 'status' => 200]);
     }
 
+
     public function store(Request $req)
-    {
-        $validator = Validator::make($req->all(), [
-            'SN' => 'required',
-            'LN' => 'required',
-            'Logo' => 'required',
-            'Description' => 'required|string',
-            'SecteurActivité' => 'required|string',
-            'Categorie' => 'required|string',
-            'Site Web' => 'required|string',
-            'Addresse mail' => 'required|email',
-            'Organigramme' => 'required|string',
-            'Network_Design' => 'required|string',
+{
+    $validator = Validator::make($req->all(), [
+        'SN' => 'required',
+        'LN' => 'required',
+        'Logo' => 'required|image|mimes:jpeg,png,jpg,gif',
+        'Organigramme' => 'required|image|mimes:jpeg,png,jpg,gif',
+        'Network_Design' => 'required|image|mimes:jpeg,png,jpg,gif',
+        'Description' => 'required|string',
+        'SecteurActivité' => 'required|string',
+        'Categorie' => 'required|string',
+        'Site_Web' => 'required|string',
+        'Addresse_mail' => 'required|email',
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json([
+            'status' => 422,
+            'validate_err' => $validator->getMessageBag(),
         ]);
-
-        if ($validator->fails()) {
-            return response()->json([
-                'status' => 422,
-                'validate_err' => $validator->getMessageBag(),
-            ]);
-        }
-
-        $item = new Customer();
-        $item->SN = $req->input('SN');
-        $item->LN = $req->input('LN');
-        $item->Logo = $req->input('Logo');
-        $item->Description = $req->input('Description');
-        $item->SecteurActivité = $req->input('SecteurActivité');
-        $item->Categorie = $req->input('Categorie');
-        $item['Site Web'] = $req->input('Site Web');
-        $item['Addresse mail']= $req->input('Addresse mail');
-        $item->Organigramme = $req->input('Organigramme');
-        $item->Network_Design = $req->input('Network_Design');
-        
-        $item->save();
-
-        return response()->json(['message' => 'Customer created successfully', 'status' => 200]);
     }
+
+    $item = new Customer();
+    $item->SN = $req->input('SN');
+    $item->LN = $req->input('LN');
+    $item->Description = $req->input('Description');
+    $item->SecteurActivité = $req->input('SecteurActivité');
+    $item->Categorie = $req->input('Categorie');
+    $item['Site Web'] = $req->input('Site_Web');
+    $item['Addresse mail' ]= $req->input('Addresse_mail');
+
+    // Handle Logo file upload
+    if ($req->hasFile('Logo')) {
+        $logoFile = $req->file('Logo');
+        $logoFileName = $item->SN . '_Logo.' . $logoFile->getClientOriginalExtension();
+        $logoFile->move(public_path('images/uploads'), $logoFileName);
+        $item->Logo = $logoFileName;
+    }
+
+    // Handle Organigramme file upload
+    if ($req->hasFile('Organigramme')) {
+        $orgFile = $req->file('Organigramme');
+        $orgFileName = $item->SN . '_Organigramme.' . $orgFile->getClientOriginalExtension();
+        $orgFile->move(public_path('images/uploads'), $orgFileName);
+        $item->Organigramme = $orgFileName;
+    }
+
+    // Handle Network Design file upload
+    if ($req->hasFile('Network_Design')) {
+        $networkFile = $req->file('Network_Design');
+        $networkFileName = $item->SN . '_NetworkDesign.' . $networkFile->getClientOriginalExtension();
+        $networkFile->move(public_path('images/uploads'), $networkFileName);
+        $item->Network_Design = $networkFileName;
+    }
+
+    $item->save();
+
+    return response()->json(['message' => 'Customer created successfully', 'status' => 200]);
+}
 
     
     public function uploadimage(Request $request)
