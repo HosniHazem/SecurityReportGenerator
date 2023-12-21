c0b86f8d1f39bdac68fa7dd502c8766e5f6de246
ahmedbenbettaieb <ahmed.benbettaieb@isimg.tn>
2023-12-21 14:39:53 +0100

    updating query backend

--- backend/app/Http/Controllers/CloneController.php
+++ backend/app/Http/Controllers/CloneController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Attribute;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
@@ -33,4 +34,66 @@ class CloneController extends Controller
             return response()->json(['error' => 'Table does not exist'], 404);
         }
     }
+
+    public function Modify(Request $request){
+        $tableName = $request->name;
+        $attribute=$request->attribute;
+        $value=$request->value;
+        $id=$request->id;
+        $tableAttributes=[];
+        $tableAttributes=self::getAttributes($tableName);
+        $primaryKey=self::getPrimaryKey($tableAttributes);
+
+
+        $sqlUpdate = 'UPDATE ' . $tableName . ' SET ' . $attribute . ' = \'' . $value . '\' WHERE ' . $primaryKey . ' = \'' . $id  . '\'';
+        
+        
+        
+        try {
+            DB::statement($sqlUpdate);
+    
+            return response()->json(['success' => 'Query executed successfully']);
+        } catch (\Exception $e) {
+            // Handle query execution failure
+            return response()->json(['error' => $e->getMessage()]);
+        }
+
+    }
+
+
+
+    static function getAttributes($tableName)
+    {
+
+        // Check if the table exists
+        if (Schema::hasTable($tableName)) {
+            // Table exists, fetch all attributes with their data
+            $columnNames = Schema::getColumnListing($tableName);
+            return $columnNames;
+        } else {
+            // Table doesn't exist
+            return response()->json(['error' => 'Table does not exist'], 404);
+        }
+    }
+    
+    static function getPrimaryKey($tableAttributes){
+
+        $primaryKey="";
+        // return response()->json($tableAttributes);
+        for($i=0;$i<count($tableAttributes);$i++){
+
+            if($tableAttributes[$i]=='id'){
+                $primaryKey='id';
+                break;
+            }
+            else if($tableAttributes[$i]=='ID'){
+                $primaryKey='ID';
+                break;
+            }
+
+        }
+        return $primaryKey;
+    }
+
+
 }
