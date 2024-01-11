884c6b3339ae1c4f72dba39b4cdf1dd446326df7
ahmedbenbettaieb <ahmed.benbettaieb@isimg.tn>
2024-01-11 16:00:50 +0100

    updating anomalie form

--- frontend/src/Anomalie/index.jsx
+++ frontend/src/Anomalie/index.jsx
@@ -93,84 +93,104 @@ useEffect(()=>{
 
   const onFinish = async (values) => {
     console.log(values);
-
+  
     try {
-      const responseQuery = await axiosInstance.post(
-        "/get-vuln",
-        {
-          q: values.q,
-          id: id,
-        },
-        {
-          headers: {
-            "X-Auth":
-              "1986ad8c0a5b3df4d7028d5f3c06e936c1dec77fb40364d109ff9c6b70f27bc4a",
+      if (values.q) {
+        const responseQuery = await axiosInstance.post(
+          "/get-vuln",
+          {
+            q: values.q,
+            id: id,
           },
+          {
+            headers: {
+              "X-Auth":
+                "1986ad8c0a5b3df4d7028d5f3c06e936c1dec77fb40364d109ff9c6b70f27bc4a",
+            },
+          }
+        );
+  
+        console.log(responseQuery.data);
+  
+        if (responseQuery.data.success) {
+          toast.success(responseQuery.data.message);
+          setAccunetixNumber(responseQuery.data.data);
+        } else {
+          toast.error("wrong");
         }
-      );
-
-      console.log(responseQuery.data);
-
-      if (responseQuery.data.success) {
-        toast.success(responseQuery.data.message);
-        setAccunetixNumber(responseQuery.data.data);
-      } else {
-        toast.error("wrong");
-      }
-
-      // Send the file1 value to the first endpoint with id
-      const formDataFile1 = new FormData();
-      formDataFile1.append("vuln", values.file1[0]?.originFileObj, "vuln.html");
-      console.log(values.file1[0]?.originFileObj);
-      const responseFile1 = await axiosInstance.post(
-        `/vuln-from-html/${id}`,
-        formDataFile1,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
-
-      console.log(responseFile1.data);
-
-      if (responseFile1.data.success) {
-        toast.success(responseFile1.data.message);
-        setInvicti(responseFile1.data.number);
-      } else {
-        toast.error("wrong");
-      }
-
-      // Send the file2 value to the second endpoint without id
-      const formDataFile2 = new FormData();
-      formDataFile2.append("vuln", values.file2[0]?.originFileObj);
-
-      const responseFile2 = await axiosInstance.post(
-        `/vuln-from-hcl/${id}`,
-        formDataFile2,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
-
-      console.log(responseFile2.data);
-
-      if (responseFile2.data.success) {
-        toast.success(responseFile2.data.message);
-        setHcl(responseFile2.data.number);
-        showModal();
-      } else {
-        toast.error("wrong");
-      }
-      if (responseFile1.data.success) {
-        console.log("smodal", isModalVisible);
       }
     } catch (error) {
-      toast.error("something wrong");
+      toast.error("something wrong with query");
+      console.log(error);
     }
+  
+    try {
+      // Send the file1 value to the first endpoint with id
+      if (values.file1 && values.file1[0].originFileObj) {
+        const formDataFile1 = new FormData();
+        formDataFile1.append("vuln", values.file1[0]?.originFileObj, "vuln.html");
+  
+        const responseFile1 = await axiosInstance.post(
+          `/vuln-from-html/${id}`,
+          formDataFile1,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
+  
+        console.log(responseFile1.data);
+  
+        if (responseFile1.data.success) {
+          toast.success(responseFile1.data.message);
+          setInvicti(responseFile1.data.number);
+        } else {
+          toast.error("wrong");
+        }
+      }
+    } catch (error) {
+      toast.error("something wrong with file1");
+      console.log(error);
+    }
+  
+    try {
+      // Send the file2 value to the second endpoint without id
+      if (values.file2 && values.file2[0]?.originFileObj) {
+        const formDataFile2 = new FormData();
+        formDataFile2.append("vuln", values.file2[0]?.originFileObj);
+    
+        const responseFile2 = await axiosInstance.post(
+          `/vuln-from-hcl/${id}`,
+          formDataFile2,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
+    
+        console.log(responseFile2.data);
+    
+        if (responseFile2.data.success) {
+          toast.success(responseFile2.data.message);
+          setHcl(responseFile2.data.number);
+          showModal();
+        } else {
+          toast.error("wrong");
+        }
+      } else {
+        console.log("File 2 is not present. Skipping the request.");
+      }
+    } catch (error) {
+      toast.error("something wrong with file2");
+      console.log(error);
+    }
+    
+    
   };
+  
+  
 
 
 
@@ -220,7 +240,7 @@ useEffect(()=>{
             <Col span={8}>
               <Form.Item
                 name="file1"
-                label="HTML"
+                label="Invicti"
                 valuePropName="fileList"
                 getValueFromEvent={normHtmlFile}
               >
