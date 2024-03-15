a19a474022bf3b324398293b284700de08b468a1
ahmedbenbettaieb <ahmed.benbettaieb@isimg.tn>
2024-03-15 10:26:14 +0100

    update axios instance

--- frontend/src/axios/axiosInstance.js
+++ frontend/src/axios/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
  export const axiosInstance=axios.create({
-    baseURL:'http://webapp.smartskills.tn/AppGenerator/backend/api',
+    baseURL:'http://webapp.ssk.lc/AppGenerator/backend/api',
     headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
 
